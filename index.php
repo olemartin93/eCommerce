02804cedf02601e669c799ead4de1714f442549b
olemartin93 <ohelgesen39@gmail.com>
2018-04-09 15:02:14 +0200

    added project from other computer

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<?php include_once('php/head.php'); ?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<a class="navbar-brand" href="#"><img src="eZone.png" alt="" srcset="" height="60" width="60"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">League of Legends</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Counter Strike</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">World of Warcraft</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fortnite</a>
+        <li class="nav-item">
+        <a class="nav-link" href="#">PUBG</a>
+      </li>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<div class="jumbotron">
+  <h1 class="display-3">League of Legends</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Read more</a>
+  </p>
+</div>
+
+<div class="card border-primary mb-3" style="max-width: 20rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Primary card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
+<body>
+    
+</body>
+</html>
---
+++ php/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>eZone</title>
+    <!-- henter thema fra CDN:  https://bootswatch.com/cyborg/ -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/cyborg/bootstrap.min.css">
+</head>
+<body>
